bdfe1f2b14891a884e6cccbb943a780336106f95
hieukhelau12 <tuminhhieu195@gmail.com>
2025-11-11 21:39:08 +0700

    update drag deck

--- scripts/card_ui/card_states/card_released_state.gd
+++ scripts/card_ui/card_states/card_released_state.gd
@@ -1,7 +1,17 @@
 extends CardState
 
+var player: bool
 
 func enter() -> void:
 	card_ui.color.color = Color.DARK_VIOLET
 	card_ui.state.text = "RELEASED"
+	player = false
 	
+	if not card_ui.targets.is_empty():
+		player = true
+
+func on_input(_event: InputEvent) -> void:
+	if player:
+		return
+	
+	transition_requested.emit(self, CardState.State.BASE)
--- scripts/card_ui/card_ui.gd
+++ scripts/card_ui/card_ui.gd
@@ -8,6 +8,8 @@ signal reparent_requested(which_card_ui: CardUI)
 @onready var state: Label = $State
 @onready var drop_point_detector: Area2D = $DropPointDetector
 @onready var card_state_machine: CardStateMachine = $CardStateMachine	as CardStateMachine
+@onready var targets: Array[Node] = []
+
 
 func _ready() -> void:
 	card_state_machine.init(self)
@@ -31,3 +33,12 @@ func _on_mouse_entered() -> void:
 
 func _on_mouse_exited() -> void:
 	pass # Replace with function body.
+
+
+func _on_drop_point_detector_area_entered(area: Area2D) -> void:
+	if not targets.has(area):
+		targets.append(area)
+
+
+func _on_drop_point_detector_area_exited(area: Area2D) -> void:
+	targets.erase(area)
